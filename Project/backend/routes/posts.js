717470489cad270c08a8d01d0aede6d6299fad5d
Ritik <104757439+ritikkatiyar11@users.noreply.github.com>
2023-11-14 00:32:24 +0530

    Update posts.js

--- Project/backend/routes/posts.js
+++ Project/backend/routes/posts.js
@@ -7,7 +7,7 @@ const Comment=require('../models/Comment')
 const verifyToken = require('../verifyToken')
 
 //CREATE
-router.post("/create",verifyToken,async (req,res)=>{
+router.post("/create",async (req,res)=>{
     try{
         const newPost=new Post(req.body)
         // console.log(req.body)
