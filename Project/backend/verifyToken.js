605bdacf5c00b3eb6348e55ce304830685cc794e
Ritik <104757439+ritikkatiyar11@users.noreply.github.com>
2023-11-14 17:12:39 +0530

    Update verifyToken.js

--- Project/backend/verifyToken.js
+++ Project/backend/verifyToken.js
@@ -2,9 +2,9 @@ const jwt=require('jsonwebtoken')
 
 const verifyToken=(req,res,next)=>{
     const token=req.cookies.token
-    // console.log(token)
+    console.log(token)
     if(!token){
-        return res.status(401).json("You are not authenticated!"+console.log(req.body)+" jug"+console.log(req.cookies))
+        return res.status(401).json("You are not authenticated!"))
     }
     jwt.verify(token,process.env.SECRET,async (err,data)=>{
         if(err){
